7460e7641dd35d5a89df5be4b4ec621da8571206
admin <trungpro151224@gmail.com>
2018-09-25 14:14:32 +0700

    features/modeling-users: validate with user in app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_084418) do
+ActiveRecord::Schema.define(version: 2018_09_25_070502) do
 
   create_table "matches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "notes"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2018_09_24_084418) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "yards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,19 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+    test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  
 end
