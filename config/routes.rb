1ba5b7f0a82b7b04d8aeffd72cb175ed06c87532
admin <trungpro151224@gmail.com>
2018-09-25 10:57:06 +0700

    develop: update menu page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  
-  resources :posts
+
   root 'menu#show'
-  resources :matches
-  resources :yards
-  resources :teams
   resources :users
   resources :posts
 end
