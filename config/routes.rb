905082ce4a72e6b253b436750ed6c60b82ab6f0e
admin <trungpro151224@gmail.com>
2018-09-26 00:23:34 +0700

    features/basic-login: login with session rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   root 'menu#show'
   get  '/help',    to: 'menu#help'
   get  '/about',    to: 'menu#about'
@@ -8,6 +9,10 @@ Rails.application.routes.draw do
   get  '/history',    to: 'menu#history'
   get  '/sign_up',  to: 'users#new'
   post '/sign_up',  to: 'users#create'
+  get  '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :users
   resources :posts
 end
