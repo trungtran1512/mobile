0f1be9f5e741b0b88bb1a06186f7b40de0cc28f5
admin <trungpro151224@gmail.com>
2018-09-25 16:43:56 +0700

    features/sign-up: create sign up in app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,29 @@ class UsersController < InheritedResources::Base
     @users = User.all
   end
 
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  @user = User.new(user_params)    # Not the final implementation!
+  if @user.save
+    flash[:success] = "Create user done!"
+    redirect_to @user
+    # Handle a successful save.
+  else
+    render 'new'
+  end
+end
+
   private
 
     def user_params
-      params.require(:user).permit(:name, :email)
+       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
   root 'menu#show'
-  get  'menu/help'
-  get  'menu/about'
-  get 'menu/contact'
-  get 'menu/store'
-  get 'menu/history'
-  get  '/signup',  to: 'users#new'
+  get  '/help',    to: 'menu#help'
+  get  '/about',    to: 'menu#about'
+  get  '/contact',    to: 'menu#contact'
+  get  '/store',    to: 'menu#store'
+  get  '/history',    to: 'menu#history'
+  get  '/sign_up',  to: 'users#new'
+  post '/sign_up',  to: 'users#create'
   resources :users
   resources :posts
 end
