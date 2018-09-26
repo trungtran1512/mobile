b184032ab920529da42dd0b73aaa68e67d69074d
admin <trungpro151224@gmail.com>
2018-09-26 11:38:07 +0700

    features/advanced-login: login with remember me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_070502) do
+ActiveRecord::Schema.define(version: 2018_09_26_021507) do
 
   create_table "matches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "notes"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2018_09_25_070502) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
